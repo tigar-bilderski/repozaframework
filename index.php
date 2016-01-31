241b4be796cf1d00369f1a953be0f62668ac6970
savicdragan15 <savicdragan2707@gmail.com>
2016-01-31 19:58:48 +0100

    Merge origin/jolle into gandra
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -2,12 +2,3 @@
 require __DIR__ . '/vendor/autoload.php';
 require_once 'config/config.php';
 require_once 'libs/init.php';
-
-//asdsadsadsad
-//jos nseto
-//sadsadsa
-///eeee
-///pozdrav
-//zdravpo2222
-///ojhahaaa
-//sadsadsa
