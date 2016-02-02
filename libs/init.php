6d1b4f4eae5d4ab6d03bd1b65ef6201bd4491226
savicdragan15 <savicdragan2707@gmail.com>
2016-02-02 20:47:48 +0100

    init i htaccess

--- libs/init.php
+++ libs/init.php
@@ -2,16 +2,19 @@
 require_once "Loader.php";
 include_once realpath("controllers/baseController.php");
 include_once realpath("models/baseModel.php");
-$controller=(isset($_GET['c']))?$_GET['c']:_DEFAULT_CONTROLLER;
-$method=(isset($_GET['f']))?$_GET['f']:"index";
+var_dump($url = explode("/",$_GET['rt']));
+$controller=(isset($_GET['rt']))?$url[0]:_DEFAULT_CONTROLLER;
+$method=(isset($_GET['rt']))?$url[1]:"index";
+
 $path_to_controller=realpath("controllers/".strtolower($controller)."Controller.php");
 if (file_exists($path_to_controller))
 {
 include_once $path_to_controller;
 $controller=strtolower($controller)."Controller";
-unset($_GET['c']);
-unset($_GET['f']);
-$params=$_GET;
+unset($url[0]);
+unset($url[1]);
+$params=$url;
+var_dump($params);
 Loader::loadController($controller,$method,$params);
 }
 else
