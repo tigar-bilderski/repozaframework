7dd13f2dd7762d90d05e3110427bf064b4fee4ce
savicdragan15 <savicdragan2707@gmail.com>
2016-02-21 20:34:57 +0100

    Changes

--- modules/registracija/controllers/registracija.php
+++ modules/registracija/controllers/registracija.php
@@ -4,7 +4,6 @@ class Registracija extends baseController
 {
     public function index()
     {
-        $model = new RegistracijaModel();
         Loader::loadModel($this, "registracija", "registracija");
         if(isset($_POST['name'])){
             $this->models['registracija']->insert($_POST['name'],$_POST['email'],$_POST['password']);
