28197823864569ecaee7b6ec8bc563b630da0e6b
Dragan <Dragan@Dragan-PC>
2016-03-21 19:02:31 +0100

    Added classess: Session and Cookie. Created module login. Multi languages translate (Serbian ,English ,German ,France )

--- views/headerView.php
+++ views/headerView.php
@@ -6,24 +6,25 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
+        <!-- CSS INCLUDE -->        
+        <link rel="stylesheet" type="text/css" id="theme" href="<?=_WEB_PATH?>views/css/theme-default.css"/>
+        <link rel="stylesheet" type="text/css" id="theme" href="<?=_WEB_PATH?>views/js/plugins/alertify/css/alertify.css"/>
+        <link rel="stylesheet" type="text/css" id="theme" href="<?=_WEB_PATH?>views/js/plugins/alertify/css/themes/bootstrap.css"/>
+        <!-- EOF CSS INCLUDE --> 
+        <!-- FLAG ICON-->
+         <link rel="stylesheet" type="text/css" href="<?=_WEB_PATH?>views/css/iconFlag/flag-icon.min.css">
+         <link rel="stylesheet" type="text/css" href="<?=_WEB_PATH?>views/assets/docs.css">
+         <script src="<?=_WEB_PATH?>/views/js/docs.js"></script>
+         <!-- EOF FLAG ICON -->
         <link rel="icon" href="<?=_WEB_PATH?>views/favicon.ico" type="image/x-icon" />
         <!-- END META SECTION -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
          <script type="text/javascript" src="<?=_WEB_PATH?>views/js/plugins/jquery/jquery.min.js"></script>
          <script type="text/javascript" src="<?=_WEB_PATH?>views/js/plugins/alertify/alertify.js"></script>
          
-         <!-- FLAG ICON-->
-         <link rel="stylesheet" type="text/css" href="<?=_WEB_PATH?>views/css/iconFlag/flag-icon.min.css">
-         <link rel="stylesheet" type="text/css" href="<?=_WEB_PATH?>views/assets/docs.css">
-         <script src="<?=_WEB_PATH?>/views/js/docs.js"></script>
-         <!-- EOF FLAG ICON -->
          
-        <!-- CSS INCLUDE -->        
-        <link rel="stylesheet" type="text/css" id="theme" href="<?=_WEB_PATH?>views/css/theme-default.css"/>
-        <link rel="stylesheet" type="text/css" id="theme" href="<?=_WEB_PATH?>views/js/plugins/alertify/css/alertify.css"/>
-        <link rel="stylesheet" type="text/css" id="theme" href="<?=_WEB_PATH?>views/js/plugins/alertify/css/themes/bootstrap.css"/>
-        <!-- EOF CSS INCLUDE -->                   
+         
+                          
     </head>
     <body>
         <!-- START PAGE CONTAINER -->
