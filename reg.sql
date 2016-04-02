e19789371b3ccd42981c6cc0eef9a86975c15116
savicdragan15 <savicdragan2707@gmail.com>
2016-04-02 19:24:34 +0200

    Add mudle users

--- reg.sql
+++ reg.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2016 at 07:17 PM
+-- Generation Time: Apr 02, 2016 at 07:23 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -62,29 +62,35 @@ INSERT INTO `navigation` (`ID`, `name`, `link`, `icon_class`, `nav_class`, `acti
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=20 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=24 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(9, 'Jovan Jagodic', 'jovan.jagodic@link.co.rs', 'jasamkonj'),
-(2, 'eee', 'eee@ee.com', 'eweeeee'),
-(3, 'eee', 'eee@ee.com', 'eweeeee'),
-(4, 'sadsad', 'sadsad', 'sadasxda'),
-(5, 'sadsad', 'sadsad', 'sadasxda'),
-(12, 'Dragan Savic', 'dragan.savic@link.co.rs', 'dragan'),
-(13, 'test', 'test', 'test'),
-(14, 'Dragan test', 'sad@sad.com', 'sadsadsa'),
-(15, '', '', ''),
-(16, 'or &#39;1&#39;=&#39;1&#39;', 'sadas ;;;;&#39;&#39;&#39;&#39;&#39;&#39;&#39;&#39;&#39;&#34;&#34;&#34;', '&#34;&#34;&#34;sad'),
-(17, 'Goran Mladenovic', 'gowasdasd@sadas.com', 'sadsadsadads'),
-(18, '', '', ''),
-(19, '', '', '');
+INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `phone`) VALUES
+(9, 'Jovan Jagodic', '', 'jovan.jagodic@link.co.rs', 'jasamkonj', ''),
+(2, 'eee', '', 'eee@ee.com', 'eweeeee', ''),
+(3, 'eee', '', 'eee@ee.com', 'eweeeee', ''),
+(4, 'sadsad', '', 'sadsad', 'sadasxda', ''),
+(5, 'sadsad', '', 'sadsad', 'sadasxda', ''),
+(12, 'Dragan Savic', '', 'dragan.savic@link.co.rs', 'dragan', ''),
+(13, 'test', '', 'test', 'test', ''),
+(14, 'Dragan test', '', 'sad@sad.com', 'sadsadsa', ''),
+(15, '', '', '', '', ''),
+(16, 'or &#39;1&#39;=&#39;1&#39;', '', 'sadas ;;;;&#39;&#39;&#39;&#39;&#39;&#39;&#39;&#39;&#39;&#34;&#34;&#34;', '&#34;&#34;&#34;sad', ''),
+(17, 'Goran Mladenovic', '', 'gowasdasd@sadas.com', 'sadsadsadads', ''),
+(18, '', '', '', '', ''),
+(19, '', '', '', '', ''),
+(20, 'Dragan', 'Savic', 'savicdragan2707@gmail.com', '20df3ed532cf7ed6def23dd49ad214b3', '+381637625902'),
+(21, 'Jovan', 'Jagodic', 'jovan.jagodic@gmail.com', 'b59c6e9b344bae1a36fe427a42889265', '+38161589226'),
+(22, 'Jovan', 'Jagodic', 'jovan.jagodic@gmail.com', 'b59c6e9b344bae1a36fe427a42889265', '+38161589226'),
+(23, 'Jovan', 'Jagodic', 'jovan.jagodic@gmail.com', 'b59c6e9b344bae1a36fe427a42889265', '+38161589226');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
